deb34d1db4c8db5dfeab2a4e668170cee5ec2252
MaZilla <pvs.mazilla@gmail.com>
2026-08-29 00:53:43 +0300

    - проведен рефакторинг кода.

--- 3/userfs.cpp
+++ 3/userfs.cpp
@@ -75,16 +75,16 @@ ufs_errno()
 	return ufs_error_code;
 }
 
-static file *create_ufs_file(const std::string& filename){
+static file
+*create_ufs_file(const std::string& filename){
 	file *new_file = new file{};
 	new_file->name = filename;
-	block *new_block = new block{};
-	rlist_add_tail_entry(&new_file->blocks, new_block, in_block_list);
 	rlist_add_tail_entry(&file_list, new_file, in_file_list);
 	return new_file;
 }
 
-static file *get_ufs_file(const std::string& filename){
+static file
+*get_ufs_file(const std::string& filename){
 	rlist *item{};
 	rlist_foreach(item, &file_list){
 		file* f = rlist_entry(item, struct file, in_file_list);
@@ -93,7 +93,8 @@ static file *get_ufs_file(const std::string& filename){
 	return nullptr;
 }
 
-static filedesc *create_ufs_filedesc(file *opened_file, int flags){
+static filedesc
+*create_ufs_filedesc(file *opened_file, int flags){
 	filedesc *new_filedesc = new filedesc{};
 	new_filedesc->mode = flags;
 	block* blk = rlist_first_entry(&opened_file->blocks, struct block, in_block_list);
@@ -105,7 +106,18 @@ static filedesc *create_ufs_filedesc(file *opened_file, int flags){
 	return new_filedesc;
 }
 
-static void add_new_empty_block(filedesc *fd){
+static void
+create_init_block(filedesc *fd){
+	block *new_block = new block{};
+	memset(new_block->memory, 0, BLOCK_SIZE);
+	rlist_add_tail_entry(&fd->atfile->blocks, new_block, in_block_list);
+	fd->block_ptr = new_block;
+	fd->block_num = 0;
+	fd->offset = 0;
+}
+
+static void
+add_new_empty_block(filedesc *fd){
 	block *new_block = new block{};
 	rlist_add_tail_entry(&fd->atfile->blocks, new_block, in_block_list);
 	fd->block_ptr = new_block;
@@ -113,20 +125,16 @@ static void add_new_empty_block(filedesc *fd){
 	fd->offset = 0;
 }
 
-static void update_file_size(filedesc *fd){
+static void
+update_file_size(filedesc *fd){
 	size_t new_file_size = static_cast<size_t>(BLOCK_SIZE) * fd->block_num + fd->offset;
 	if(new_file_size > fd->atfile->size){
 		fd->atfile->size = new_file_size;
 	}
 }
 
-static filedesc *get_filedesc(int fd){
-	if(fd < 0 || static_cast<size_t>(fd) >= file_descriptors.size()) {
-		return nullptr;
-	}
-	filedesc *desc = file_descriptors[fd];
-	if(!desc) return nullptr;
-	
+static void
+actualize_file_descriptor(filedesc *desc){
 	size_t sz = desc->block_num * static_cast<size_t>(BLOCK_SIZE) + desc->offset;
 	if(sz > desc->atfile->size){
 		desc->offset = desc->atfile->size % static_cast<size_t>(BLOCK_SIZE);
@@ -144,14 +152,32 @@ static filedesc *get_filedesc(int fd){
 			++cnt;
 		}
 	} else {
-		block *new_block = new block{};
-		memset(new_block->memory, 0, BLOCK_SIZE);
-		rlist_add_tail_entry(&desc->atfile->blocks, new_block, in_block_list);
-		desc->block_ptr = new_block;
+		desc->block_ptr = nullptr;
 		desc->block_num = 0;
 		desc->offset = 0;
 	}
+}
+
+static filedesc 
+*get_filedesc(int fd){
+	if(fd < 0 || static_cast<size_t>(fd) >= file_descriptors.size()) {
+		return nullptr;
+	}
+	filedesc *desc = file_descriptors[fd];
+	if(!desc) return nullptr;
+	
+	actualize_file_descriptor(desc);
 	return desc;
+}
+
+static int
+find_free_filedescriptor(){
+	for(int fd = 0; static_cast<size_t>(fd) < file_descriptors.size(); ++fd){
+		if(file_descriptors[fd] == nullptr){
+			return fd;
+		}
+	}
+	return -1;
 }
 
 int
@@ -170,13 +196,7 @@ ufs_open(const char *filename, int flags)
 	if(flags == UFS_CREATE) flags |= UFS_READ_WRITE;
 #endif
 
-	int fd{-1};
-	for(; static_cast<size_t>(fd) < file_descriptors.size(); ++fd){
-		if(file_descriptors[fd + 1] == nullptr){
-			++fd;
-			break;
-		}
-	}
+	auto fd = find_free_filedescriptor();
 
 	filedesc *desc = create_ufs_filedesc(opened_file, flags);
 	if(fd < 0){
@@ -186,6 +206,28 @@ ufs_open(const char *filename, int flags)
 	
 	file_descriptors[fd] = desc;
 	return fd;
+}
+
+static ssize_t
+ufs_write_data_to_current_block(filedesc *desc, const char *buf, size_t size){
+	if(size > BLOCK_SIZE - desc->offset) return -1;
+	memcpy((desc->block_ptr->memory + desc->offset), buf, size);
+	desc->offset += size;
+	update_file_size(desc);
+	return size;
+}
+
+static void
+force_shift_to_next_block(filedesc *desc){
+	if(desc->block_ptr->in_block_list.next == &desc->atfile->blocks){
+		add_new_empty_block(desc);
+		return;
+	}
+
+	block* blk = rlist_entry(desc->block_ptr->in_block_list.next, struct block, in_block_list);	
+	desc->block_ptr = blk;
+	desc->block_num++;
+	desc->offset = 0;
 }
 
 ssize_t
@@ -204,6 +246,10 @@ ufs_write(int fd, const char *buf, size_t size)
 	}
 #endif
 
+	if(desc->atfile->size == 0 && !desc->block_ptr && size != 0){
+		create_init_block(desc);
+	}
+
 	size_t current_pos = desc->block_num * BLOCK_SIZE + desc->offset;
 	if (current_pos + size > MAX_FILE_SIZE) {
 		ufs_error_code = UFS_ERR_NO_MEM;
@@ -211,39 +257,41 @@ ufs_write(int fd, const char *buf, size_t size)
 	}
 
 	if(size <= BLOCK_SIZE - desc->offset){
-		memcpy((desc->block_ptr->memory + desc->offset), buf, size);
-		desc->offset += size;
-		update_file_size(desc);
-		return size;
+		return ufs_write_data_to_current_block(desc, buf, size);
 	}
 
-	size_t total_size{BLOCK_SIZE - desc->offset};
+	size_t total_size = ufs_write_data_to_current_block(desc, buf, BLOCK_SIZE - desc->offset);
 	size_t new_size{};
-	memcpy((desc->block_ptr->memory + desc->offset), buf, BLOCK_SIZE - desc->offset);
-	desc->offset = BLOCK_SIZE;
 
 	while(total_size < size){
-		if (desc->offset == BLOCK_SIZE) {
-			if(desc->block_ptr->in_block_list.next == &desc->atfile->blocks){
-				add_new_empty_block(desc);
-			} else {
-				block* blk = rlist_entry(desc->block_ptr->in_block_list.next, struct block, in_block_list);	
-				desc->block_ptr = blk;
-				desc->block_num++;
-				desc->offset = 0;
-			}
-		}
-
+		if (desc->offset == BLOCK_SIZE) force_shift_to_next_block(desc);
 		size_t diff{size - total_size};
 		new_size = static_cast<size_t>(BLOCK_SIZE - desc->offset) >= diff ? diff : static_cast<size_t>(BLOCK_SIZE - desc->offset);
-		
-		memcpy(desc->block_ptr->memory + desc->offset, buf + total_size, new_size);
-		desc->offset += new_size;
-		total_size += new_size;	
+		total_size += ufs_write_data_to_current_block(desc, buf + total_size, new_size);	
 	}
 
 	update_file_size(desc);
 	return total_size;
+}
+
+static ssize_t
+ufs_read_data_to_current_block(filedesc *desc, char *buf, size_t size){
+	if(BLOCK_SIZE - desc->offset < size) return -1;
+	if(size == 0) return 0;
+
+	memcpy(buf, (desc->block_ptr->memory + desc->offset), size);
+	desc->offset += size;
+	return size;
+}
+
+static bool
+soft_shift_to_next_block(filedesc *desc){
+	if (desc->offset != BLOCK_SIZE) return true;
+	if (desc->block_ptr->in_block_list.next == &desc->atfile->blocks) return false;
+	desc->block_ptr = rlist_entry(desc->block_ptr->in_block_list.next, struct block, in_block_list);
+	desc->block_num++;
+	desc->offset = 0;
+	return true;
 }
 
 ssize_t
@@ -262,44 +310,43 @@ ufs_read(int fd, char *buf, size_t size)
 	}
 #endif
 
-	size_t rsize = std::min(desc->atfile->size - (static_cast<size_t>(BLOCK_SIZE) * desc->block_num) - desc->offset, size);
-	if(BLOCK_SIZE - desc->offset >= rsize){
-		if(rsize == 0){
-			return 0;
-		}
-		memcpy(buf, (desc->block_ptr->memory + desc->offset), rsize);
-		desc->offset += rsize;
-		return rsize;
+	if(!desc->block_ptr) return 0;
+
+	ssize_t rsize = std::min(desc->atfile->size - (static_cast<size_t>(BLOCK_SIZE) * desc->block_num) - desc->offset, size);
+	if(static_cast<ssize_t>(BLOCK_SIZE - desc->offset) >= rsize){
+		return ufs_read_data_to_current_block(desc, buf, rsize);
 	}
 
-	size_t total_size{BLOCK_SIZE - desc->offset};
-	if(rsize == 0){
-		return 0;
-	}
-	memcpy(buf, (desc->block_ptr->memory + desc->offset), BLOCK_SIZE - desc->offset);
-	desc->offset = BLOCK_SIZE;
+	if(rsize == 0) return 0;
+	auto total_size = ufs_read_data_to_current_block(desc, buf, BLOCK_SIZE - desc->offset);
 
 	while(total_size < rsize){
 		if (desc->offset == BLOCK_SIZE) {
-			if (desc->block_ptr->in_block_list.next == &desc->atfile->blocks) {
-				break;
-			}
-			desc->block_ptr = rlist_entry(desc->block_ptr->in_block_list.next, struct block, in_block_list);
-			desc->block_num++;
-			desc->offset = 0;
+			if (!soft_shift_to_next_block(desc)) break;
 		}
-
-		size_t diff{rsize - total_size};
-		size_t blk_sz = static_cast<size_t>(BLOCK_SIZE - desc->offset) >= diff ? diff : static_cast<size_t>(BLOCK_SIZE - desc->offset);
-		
-		memcpy(buf + total_size, desc->block_ptr->memory + desc->offset, blk_sz);
-		desc->offset += blk_sz;
-		total_size += blk_sz;
+		ssize_t diff{rsize - total_size};
+		size_t blk_sz = static_cast<ssize_t>(BLOCK_SIZE - desc->offset) >= diff ? diff : static_cast<size_t>(BLOCK_SIZE - desc->offset);
+		total_size += ufs_read_data_to_current_block(desc, buf + total_size, blk_sz);
 	}
 
 	return total_size;
 }
 
+static void
+delete_blocks_in_file(file *f){
+	struct block *tmp{};
+	struct block *blk{};
+	rlist_foreach_entry_safe(blk, &f->blocks, in_block_list, tmp){
+		rlist_del_entry(blk, in_block_list);
+		delete blk;
+	}
+}
+
+static void
+delete_file(file *f){
+	delete_blocks_in_file(f);
+	delete f;
+}
 
 int
 ufs_close(int fd)
@@ -316,13 +363,7 @@ ufs_close(int fd)
 	file_descriptors[fd] = nullptr;
 	
 	if(f && f->refs == 0 && f->is_deleted){
-		struct block *tmp{};
-		struct block *blk{};
-		rlist_foreach_entry_safe(blk, &f->blocks, in_block_list, tmp){
-			rlist_del_entry(blk, in_block_list);
-			delete blk;
-		}
-		delete f;
+		delete_file(f);
 	}
 	return 0;
 }
@@ -336,18 +377,58 @@ ufs_delete(const char *filename)
 	rlist_del_entry(opened_file, in_file_list);
 
 	if(opened_file->refs == 0) {
-		struct block *tmp{};
-		struct block *blk{};
-		rlist_foreach_entry_safe(blk, &opened_file->blocks, in_block_list, tmp){
-			rlist_del_entry(blk, in_block_list);
-			delete blk;
-		}
-		delete opened_file;
+		delete_file(opened_file);
 	}
 	return 0;
 }
 
 #if NEED_RESIZE
+
+static void
+expand_file(file *f, size_t new_size){
+	if(f->size >= new_size) return;
+
+	size_t sz = f->size - (f->size % static_cast<size_t>(BLOCK_SIZE)) + static_cast<size_t>(BLOCK_SIZE);
+	if(new_size <= sz){
+		f->size = new_size;
+		return;
+	}
+		
+	while(sz < new_size){
+		block *new_block = new block{};
+		memset(new_block->memory, 0, BLOCK_SIZE);
+		rlist_add_tail_entry(&f->blocks, new_block, in_block_list);
+		sz += static_cast<size_t>(BLOCK_SIZE);
+	}
+	f->size = new_size;
+}
+
+static void
+shrink_file(file *f, size_t new_size){
+	size_t sz = f->size - (f->size % static_cast<size_t>(BLOCK_SIZE));
+	if(sz <= new_size){
+		f->size = new_size;
+		return;
+	}
+	sz += static_cast<size_t>(BLOCK_SIZE);
+	
+	block *blk = rlist_last_entry(&f->blocks, struct block, in_block_list);
+	
+	while(sz > new_size){
+		rlist *tmp = blk->in_block_list.prev;
+		
+		if (tmp == &f->blocks) {
+			break;
+		}
+
+		rlist_del_entry(blk, in_block_list);
+		delete blk;
+		
+		blk = rlist_entry(tmp, struct block, in_block_list);
+		sz -= static_cast<size_t>(BLOCK_SIZE);
+	}
+	f->size = new_size;
+}
 
 int
 ufs_resize(int fd, size_t new_size)
@@ -371,66 +452,31 @@ ufs_resize(int fd, size_t new_size)
 #endif
 
 	file *f = desc->atfile;
-
 	if(f->size == new_size) return 0;
 
 	if(f->size < new_size){
-		size_t sz = f->size - (f->size % static_cast<size_t>(BLOCK_SIZE)) + static_cast<size_t>(BLOCK_SIZE);
-		if(new_size <= sz){
-			f->size = new_size;
-			return 0;
-		}
-		
-		while(sz < new_size){
-			block *new_block = new block{};
-			memset(new_block->memory, 0, BLOCK_SIZE);
-			rlist_add_tail_entry(&f->blocks, new_block, in_block_list);
-			sz += static_cast<size_t>(BLOCK_SIZE);
-		}
-		f->size = new_size;
+		expand_file(f, new_size);
 		return 0;
 	}
-
-	size_t sz = f->size - (f->size % static_cast<size_t>(BLOCK_SIZE));
-	if(sz <= new_size){
-		f->size = new_size;
-		return 0;
-	}
-	sz += static_cast<size_t>(BLOCK_SIZE);
 	
-	block *blk = rlist_last_entry(&f->blocks, struct block, in_block_list);
-	
-	while(sz > new_size){
-		rlist *tmp = blk->in_block_list.prev;
-		
-		if (tmp == &f->blocks) {
-			break;
-		}
-
-		rlist_del_entry(blk, in_block_list);
-		delete blk;
-		
-		blk = rlist_entry(tmp, struct block, in_block_list);
-		sz -= static_cast<size_t>(BLOCK_SIZE);
-	}
-	f->size = new_size;
+	shrink_file(f, new_size);
 	return 0;
 }
 
 #endif
 
+/*
+ * The file_descriptors array is likely to leak even if
+ * you resize it to zero or call clear(). This is because
+ * the vector keeps memory reserved in case more elements
+ * would be added.
+ *
+ * The recommended way of freeing the memory is to swap()
+ * the vector with a temporary empty vector.
+ */
 void
 ufs_destroy(void)
 {
-	/*
-	 * The file_descriptors array is likely to leak even if
-	 * you resize it to zero or call clear(). This is because
-	 * the vector keeps memory reserved in case more elements
-	 * would be added.
-	 *
-	 * The recommended way of freeing the memory is to swap()
-	 * the vector with a temporary empty vector.
-	 */
 	for(int fd = 0; static_cast<size_t>(fd) < file_descriptors.size(); ++fd){
 		ufs_close(fd);
 	}
@@ -440,14 +486,7 @@ ufs_destroy(void)
 	file *file_item, *tmp_file;
 	rlist_foreach_entry_safe(file_item, &file_list, in_file_list, tmp_file) {
 		file *f = rlist_entry(file_item, struct file, in_file_list);
-		
-		struct block *tmp{};
-		struct block *blk{};
-		rlist_foreach_entry_safe(blk, &f->blocks, in_block_list, tmp){
-			rlist_del_entry(blk, in_block_list);
-			delete blk;
-		}
-		
+		delete_blocks_in_file(f);
 		rlist_del_entry(f, in_file_list);
 		delete f;
 	}
