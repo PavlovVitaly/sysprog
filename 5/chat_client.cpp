e56509aa3136548495fd995582f7880dfbdba697
MaZilla <pvs.mazilla@gmail.com>
2026-09-19 18:25:28 +0300

    - реализован чат, тесты проходят

--- 5/chat_client.cpp
+++ 5/chat_client.cpp
@@ -1,27 +1,40 @@
 #include "chat.h"
 #include "chat_client.h"
 
+#include <arpa/inet.h>
 #include <cstring>
+#include <netinet/in.h>
 #include <stdlib.h>
+#include <string_view>
+#include <sys/poll.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <memory>
+#include <queue>
+#include <fcntl.h>
 
 struct chat_client {
 	/** Socket connected to the server. */
 	int socket = -1;
 	/** Array of received messages. */
-	/* ... */
+	std::queue<std::unique_ptr<chat_message>> input_buffer;
 	/** Output buffer. */
+	std::queue<std::unique_ptr<chat_message>> output_buffer;
 	/* ... */
 	/* PUT HERE OTHER MEMBERS */
+	std::string name{};
+	std::string partial_input{}; 
+	bool is_start_of_line = true; 
 };
 
 struct chat_client *
 chat_client_new(std::string_view name)
 {
-	/* Ignore 'name' param if don't want to support it for +5 points. */
-	(void)name;
-	/* IMPLEMENT THIS FUNCTION */
-	return new chat_client();
+	chat_client *client = new chat_client();
+	client->name = name;
+	return client;
 }
 
 void
@@ -29,10 +42,26 @@ chat_client_delete(struct chat_client *client)
 {
 	if (client->socket >= 0)
 		close(client->socket);
-
-	/* IMPLEMENT THIS FUNCTION */
-
+	while(!client->output_buffer.empty()){
+		client->output_buffer.front().reset();
+		client->output_buffer.pop();
+	}
+	while(!client->input_buffer.empty()){
+		client->input_buffer.front().reset();
+		client->input_buffer.pop();
+	}
 	delete client;
+}
+
+void parse_addr(std::string_view addr, std::string& ip, std::string& port){
+	auto pos = addr.find(':');
+	if(pos != std::string_view::npos){
+		ip = addr.substr(0, pos);
+		port = addr.substr(pos + 1);
+		return;
+	}
+	ip = addr;
+	port = "80";
 }
 
 int
@@ -43,19 +72,59 @@ chat_client_connect(struct chat_client *client, std::string_view addr)
 	 * 2) Create a client socket (function socket()).
 	 * 3) Connect it by the found address (function connect()).
 	 */
-	/* IMPLEMENT THIS FUNCTION */
-	(void)client;
-	(void)addr;
+	std::string ip{};
+	std::string port{};
+	parse_addr(addr,ip, port);
 
-	return CHAT_ERR_NOT_IMPLEMENTED;
+	struct addrinfo *ainfo{};
+	struct addrinfo filter;
+	memset(&filter, 0, sizeof(struct addrinfo));
+	filter.ai_family = AF_INET;
+	filter.ai_socktype = SOCK_STREAM;
+
+	int status = getaddrinfo(ip.c_str()
+						, port.c_str()
+						, &filter
+						, &ainfo);
+	if(status != 0){
+		return CHAT_ERR_NO_ADDR;
+	}
+
+	client->socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(client->socket == -1){
+		return -1;
+	}
+
+	int rc = connect(client->socket, ainfo->ai_addr, ainfo->ai_addrlen);
+	freeaddrinfo(ainfo);
+
+	if (rc == -1) {
+		close(client->socket);
+		client->socket = -1;
+		return -1; // или CHAT_ERR_SYS
+	}
+
+	int flags = fcntl(client->socket, F_GETFL, 0);
+	fcntl(client->socket, F_SETFL, flags | O_NONBLOCK);
+
+	return 0;
 }
 
 struct chat_message *
 chat_client_pop_next(struct chat_client *client)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)client;
-	return NULL;
+	if(!client || client->input_buffer.empty()) return nullptr;
+	auto res = client->input_buffer.front().release();
+	client->input_buffer.pop();
+#if NEED_AUTHOR
+	auto tmp = res->data;
+	size_t pos{};
+	if ((pos = tmp.find(":%:")) != std::string::npos){
+		res->author = tmp.substr(0, pos);
+		res->data = tmp.substr(pos + 3);
+	}
+#endif
+	return res;
 }
 
 int
@@ -69,9 +138,79 @@ chat_client_update(struct chat_client *client, double timeout)
 	 * You create one struct pollfd, fill it, call poll() on it, handle the
 	 * events (do read/write).
 	 */
-	(void)client;
-	(void)timeout;
-	return CHAT_ERR_NOT_IMPLEMENTED;
+
+	if(!client || client->socket == -1) return CHAT_ERR_NOT_STARTED;
+	pollfd fd;
+	fd.fd = client->socket;
+	fd.events = POLLIN;
+	if(!client->output_buffer.empty()){
+		fd.events |= POLLOUT;
+	}
+
+	int ms = timeout * 1000;
+
+	int status = poll(&fd, 1, ms);
+	if(status == -1) return -1;
+	bool has_data{};
+
+	if(fd.revents & POLLOUT){
+		while(!client->output_buffer.empty()){
+			auto& data = client->output_buffer.front();
+			if (data->data.empty()) {
+				client->output_buffer.pop();
+				continue;
+			}
+
+			int sent = send(client->socket, data->data.c_str(), data->data.size(), 0);			
+			if (sent > 0) {
+				has_data = true;	
+				if (static_cast<size_t>(sent) == data->data.size()) {
+					client->output_buffer.pop();
+				} else {
+					data->data.erase(0, sent); 
+					break; 
+				}
+			} else {
+				if (errno == EAGAIN || errno == EWOULDBLOCK) {
+					break; 
+				}
+				return -1;
+			}
+		}
+	}
+
+	if(fd.revents & POLLIN){
+		size_t buf_size = 4096;
+		char buf[buf_size];
+		ssize_t sz = recv(client->socket, buf, buf_size, 0);
+		while(sz > 0){
+			has_data = true;
+			client->partial_input.append(buf, sz);
+			sz = recv(client->socket, buf, buf_size, 0);
+		}
+		size_t pos;
+		while ((pos = client->partial_input.find('\n')) != std::string::npos) {
+			auto msg = std::make_unique<chat_message>();
+			msg->data = client->partial_input.substr(0, pos);
+			client->input_buffer.push(std::move(msg));
+			client->partial_input.erase(0, pos + 1);
+		}
+
+		if(sz < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+			return -1;
+		}
+		if(sz == 0){
+			close(client->socket);
+			client->socket = -1;
+			return -1;
+		}
+	}
+
+	if (status == 0 && !has_data && client->output_buffer.empty()) {
+		return CHAT_ERR_TIMEOUT;
+	}
+	
+	return 0;
 }
 
 int
@@ -83,20 +222,50 @@ chat_client_get_descriptor(const struct chat_client *client)
 int
 chat_client_get_events(const struct chat_client *client)
 {
-	/*
-	 * IMPLEMENT THIS FUNCTION - add OUTPUT event if has non-empty output
-	 * buffer.
-	 */
-	(void)client;
-	return CHAT_EVENT_INPUT;
+	if(!client || client->socket == -1) return 0;
+	int res = CHAT_EVENT_INPUT;
+	if(!client->output_buffer.empty()) res |= CHAT_EVENT_OUTPUT;
+	return res;
 }
 
 int
 chat_client_feed(struct chat_client *client, const char *msg, uint32_t msg_size)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)client;
-	(void)msg;
-	(void)msg_size;
-	return CHAT_ERR_NOT_IMPLEMENTED;
+	if(!client || client->socket == -1) return CHAT_ERR_NOT_STARTED;
+	if(!msg || msg_size == 0) return 0;
+
+#if NEED_AUTHOR	
+	std::string buf(msg, msg_size);
+	size_t start_pos = 0;
+	size_t pos;
+
+	auto data = std::make_unique<chat_message>();
+	data->author = client->name;
+	data->data = "";
+
+	while ((pos = buf.find('\n', start_pos)) != std::string::npos) {
+		if (client->is_start_of_line) {
+			data->data += client->name + ":%:";
+		}
+		
+		data->data += buf.substr(start_pos, pos - start_pos) + "\n";
+		client->is_start_of_line = true;
+		start_pos = pos + 1;
+	}
+	if (start_pos < buf.size()) {
+		if (client->is_start_of_line) {
+			data->data += client->name + ":%:";
+		}
+		data->data += buf.substr(start_pos);
+		client->is_start_of_line = false;
+	}
+
+	client->output_buffer.push(std::move(data));
+#else
+	auto data = std::make_unique<chat_message>();
+	data->data = std::string(msg, msg_size);
+	client->output_buffer.push(std::move(data));
+#endif
+
+	return 0;
 }
--- 5/chat_server.cpp
+++ 5/chat_server.cpp
@@ -5,68 +5,179 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <unordered_set>
+#include <arpa/inet.h> 
+#include <pthread.h>
+#include <sys/epoll.h>
+#include <memory>
+#include <queue>
+#include <fcntl.h>
+
+#define CLIENT_BUFFER_SIZE 1024
+#define MAX_EVENTS 100
 
 struct chat_peer {
 	/** Client's socket. To read/write messages. */
 	int socket;
 	/** Output buffer. */
-	/* ... */
+	std::string out_buffer;
 	/* PUT HERE OTHER MEMBERS */
+	std::string partial_input{};
+	std::string name;
 };
 
 struct chat_server {
 	/** Listening socket. To accept new clients. */
 	int socket = -1;
 	/** Array of peers. */
+	std::unordered_set<chat_peer *> peers{};
 	/* ... */
 	/* PUT HERE OTHER MEMBERS */
+	int epoll_desk{-1};
+	std::queue<std::unique_ptr<chat_message>> output_buffer;
+	std::queue<std::string> input_buffer;
+	bool is_start_of_line = true; 
 };
+
+static void
+close_server(chat_server *server){
+	if(!server) return;
+	for(auto& item : server->peers){
+		if(item->socket == -1) continue;
+		epoll_ctl(server->epoll_desk, EPOLL_CTL_DEL, item->socket, NULL);
+		close(item->socket);
+		delete item;
+	}
+	if(server->socket != -1) close(server->socket);
+	if(server->epoll_desk != -1) close(server->epoll_desk);
+}
+
+static void
+close_peer(chat_server *server, chat_peer *peer){
+	if(!peer) return;
+	epoll_ctl(server->epoll_desk, EPOLL_CTL_DEL, peer->socket, NULL);
+	server->peers.erase(peer);
+	delete peer;
+}
 
 struct chat_server *
 chat_server_new(void)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	return new chat_server();
+	struct chat_server *server = new chat_server();
+	
+	return server;
 }
 
 void
 chat_server_delete(struct chat_server *server)
 {
-	if (server->socket >= 0)
-		close(server->socket);
-
-	/* IMPLEMENT THIS FUNCTION */
-
+	close_server(server);
 	delete server;
 }
 
 int
 chat_server_listen(struct chat_server *server, uint16_t port)
 {
-	struct sockaddr_in addr;
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_port = htons(port);
-	/* Listen on all IPs of this machine. */
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
 	/*
 	 * 1) Create a server socket (function socket()).
 	 * 2) Bind the server socket to addr (function bind()).
 	 * 3) Listen the server socket (function listen()).
 	 * 4) Create epoll/kqueue if needed.
 	 */
-	/* IMPLEMENT THIS FUNCTION */
-	(void)server;
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	/* Listen on all IPs of this machine. */
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	return CHAT_ERR_NOT_IMPLEMENTED;
+	server->socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (server->socket == -1) {
+		return -1;
+	}
+
+	if (bind(server->socket, (struct sockaddr *) &addr, sizeof(addr)) != 0) {
+		return -1;
+	}
+	if (listen(server->socket, 128) == -1) {
+		printf("listen error = %s\n", strerror(errno));
+		return -1;
+	}
+
+	int flags = fcntl(server->socket, F_GETFL, 0);
+	fcntl(server->socket, F_SETFL, flags | O_NONBLOCK);
+
+	server->epoll_desk = epoll_create1(0);
+	if (server->epoll_desk == -1) {
+		close_server(server);
+		return -1;
+	}
+
+	struct epoll_event new_ev;
+	new_ev.data.ptr = NULL;
+	new_ev.events = EPOLLIN;
+	if (epoll_ctl(server->epoll_desk, EPOLL_CTL_ADD, server->socket, &new_ev) == -1) {
+		close_server(server);
+		return -1;
+	}
+
+	return 0;
 }
 
 struct chat_message *
 chat_server_pop_next(struct chat_server *server)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)server;
-	return NULL;
+	if(!server || server->output_buffer.empty()) return nullptr;
+	auto* res = server->output_buffer.front().release();
+	server->output_buffer.pop();
+#if NEED_AUTHOR
+	auto tmp = res->data;
+	size_t pos{};
+	if ((pos = tmp.find(":%:")) != std::string::npos){
+		res->author = tmp.substr(0, pos);
+		res->data = tmp.substr(pos + 3);
+	}
+#endif
+	return res;
+}
+
+static int
+interact(chat_server *server, chat_peer *p)
+{
+	size_t buf_size = 1024;
+	char buf[buf_size];
+	ssize_t sz = recv(p->socket, buf, buf_size, 0);
+	if(sz < 0) return -1;
+	if(sz == 0){
+		errno = 0;
+		return -1;
+	}
+
+	p->partial_input.append(buf, sz);
+	size_t pos;
+	while ((pos = p->partial_input.find('\n')) != std::string::npos) {
+		std::string raw_msg = p->partial_input.substr(0, pos);
+		
+		auto msg = std::make_unique<chat_message>();
+		msg->data = raw_msg;
+		server->output_buffer.push(std::move(msg));
+
+		for (chat_peer *other : server->peers) {
+			if (other == p) continue; 
+
+			other->out_buffer += raw_msg;
+			other->out_buffer.push_back('\n'); // Обязательный сетевой разделитель
+
+			struct epoll_event ev;
+			memset(&ev, 0, sizeof(ev));
+			ev.events = EPOLLIN | EPOLLOUT;
+			ev.data.ptr = other;
+			epoll_ctl(server->epoll_desk, EPOLL_CTL_MOD, other->socket, &ev);
+		}
+		p->partial_input.erase(0, pos + 1);
+	}
+
+	return 0;
 }
 
 int
@@ -80,14 +191,114 @@ chat_server_update(struct chat_server *server, double timeout)
 	 * 2.2) If the update was on a client-socket, then you might want to
 	 *     read/write on it.
 	 */
-	(void)server;
-	(void)timeout;
-	return CHAT_ERR_NOT_IMPLEMENTED;
+	if(!server || server->socket == -1) return CHAT_ERR_NOT_STARTED;
+
+	int ms = timeout * 1000;
+	struct epoll_event events[MAX_EVENTS];
+	int nfds = epoll_wait(server->epoll_desk, events, MAX_EVENTS, ms);
+	if (nfds == -1) {
+		return CHAT_ERR_SYS;
+	}
+	bool has_data{};
+
+	for(int i = 0; i < nfds; ++i){
+		if (events[i].data.ptr == NULL) {
+			while (true) {
+				int peer_sock = accept(server->socket, NULL, NULL);
+				if (peer_sock == -1) {
+					if (errno == EAGAIN || errno == EWOULDBLOCK) {
+						break; 
+					}
+					printf("error = %s\n", strerror(errno));
+					break;
+				}
+				
+				int flags = fcntl(peer_sock, F_GETFL, 0);
+				fcntl(peer_sock, F_SETFL, flags | O_NONBLOCK);
+
+				chat_peer *p = new chat_peer;
+				epoll_event ev;
+				ev.data.ptr = p;
+				ev.events = EPOLLIN;
+				if (epoll_ctl(server->epoll_desk, EPOLL_CTL_ADD, peer_sock, &ev) == -1) {
+					printf("error = %s\n", strerror(errno));
+					close(peer_sock);
+					delete p;
+					break;
+				}
+				p->socket = peer_sock;
+				server->peers.insert(p);
+				has_data = true;
+			}
+			continue;
+		}
+
+		chat_peer *p = static_cast<chat_peer *>(events[i].data.ptr);
+		bool drop_client{};
+
+		if(events[i].events & EPOLLIN){
+			int rc = interact(server, p);
+			if (rc == -1) {
+				if (errno != EWOULDBLOCK && errno != EAGAIN) {
+					printf("error = %s\n", strerror(errno));
+					drop_client = true;
+				}
+			} else {
+				has_data = true;
+			}
+		}
+
+		if(!drop_client && !p->out_buffer.empty()){
+			ssize_t sent = send(p->socket, p->out_buffer.c_str(), p->out_buffer.size(), 0);
+			if (sent > 0) {
+				has_data = true;
+				p->out_buffer.erase(0, sent);
+			} else if (sent == -1) {
+				if (errno != EAGAIN && errno != EWOULDBLOCK) {
+					drop_client = true;
+				}
+			}
+
+			if(p->out_buffer.empty() && !drop_client){
+				struct epoll_event ev;
+				memset(&ev, 0, sizeof(ev));
+				ev.events = EPOLLIN;
+				ev.data.ptr = p;
+				epoll_ctl(server->epoll_desk, EPOLL_CTL_MOD, p->socket, &ev);
+			}
+		}
+
+		if(drop_client) {
+			close_peer(server, p);
+			has_data = true;
+		}
+	}
+
+	while (!server->input_buffer.empty() && !server->peers.empty()) {
+		std::string fed_msg = std::move(server->input_buffer.front());
+		server->input_buffer.pop();
+		
+		for (chat_peer *item : server->peers) {
+			item->out_buffer.append(fed_msg);
+			struct epoll_event ev;
+			memset(&ev, 0, sizeof(ev));
+			ev.events = EPOLLIN | EPOLLOUT;
+			ev.data.ptr = item;
+			epoll_ctl(server->epoll_desk, EPOLL_CTL_MOD, item->socket, &ev);
+		}
+	}
+
+	if (nfds == 0 && !has_data) {
+		return CHAT_ERR_TIMEOUT;
+	}
+	
+	return 0;
 }
 
 int
 chat_server_get_descriptor(const struct chat_server *server)
 {
+	if(!server) return -1;
 #if NEED_SERVER_FEED
 	/* IMPLEMENT THIS FUNCTION if want +5 points. */
 
@@ -105,8 +316,7 @@ chat_server_get_descriptor(const struct chat_server *server)
 	 * sockets.
 	 */
 #endif
-	(void)server;
-	return -1;
+	return server->epoll_desk;
 }
 
 int
@@ -118,22 +328,44 @@ chat_server_get_socket(const struct chat_server *server)
 int
 chat_server_get_events(const struct chat_server *server)
 {
-	/*
-	 * IMPLEMENT THIS FUNCTION - add OUTPUT event if has non-empty output
-	 * buffer in any of the client-sockets.
-	 */
-	(void)server;
+	if(!server || server->epoll_desk == -1) return 0;
 	return CHAT_EVENT_INPUT;
 }
 
 int
 chat_server_feed(struct chat_server *server, const char *msg, uint32_t msg_size)
 {
-#if NEED_SERVER_FEED
-	/* IMPLEMENT THIS FUNCTION if want +5 points. */
+	if(!server || server->epoll_desk == -1) return CHAT_ERR_NOT_STARTED;
+	if(!msg || msg_size <= 0) return 0;
+
+#if NEED_AUTHOR
+	std::string buf(msg, msg_size);
+	size_t start_pos = 0;
+	size_t pos;
+	std::string full_packet = "";
+
+	while ((pos = buf.find('\n', start_pos)) != std::string::npos) {
+		if (server->is_start_of_line) {
+			full_packet += "server:%:";
+		}
+		
+		full_packet += buf.substr(start_pos, pos - start_pos) + "\n";
+		server->is_start_of_line = true;
+		start_pos = pos + 1;
+	}
+	if (start_pos < buf.size()) {
+		if (server->is_start_of_line) {
+			full_packet += "server:%:";
+		}
+		full_packet += buf.substr(start_pos);
+		server->is_start_of_line = false;
+	}
+
+	server->input_buffer.push(full_packet);
+#else
+	std::string full_packet(msg, msg_size);
+	server->input_buffer.push(full_packet);
 #endif
-	(void)server;
-	(void)msg;
-	(void)msg_size;
-	return CHAT_ERR_NOT_IMPLEMENTED;
+
+	return 0;
 }
